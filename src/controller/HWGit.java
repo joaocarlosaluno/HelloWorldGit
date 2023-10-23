b173fa3b9f9fa8b49612265262610a427bce7304
Joao Carlos <joao.c.lima8@alunos.senai.br>
2023-10-23 14:30:09 -0300

    Criacao do HelloWordGit

---
+++ src/controller/HWGit.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class HWGit {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Olá mundo");
+		System.out.println("Conhecendo o Git");
+
+	}
+
+}
